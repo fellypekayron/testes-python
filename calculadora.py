3848382ecf077a2b38d95a724204e6d5596bbd76
fellypekayron <fellypekayron14@gmail.com>
2024-11-13 16:06:32 -0300

    Arquivos inicias

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+# 1) Criar a classe 
+# chamada "Calculadora"
+class Calculadora:
+    
+    # 2) Criar um método
+    # chamado "soma()"
+    def soma(self, n1, n2):
+        return n1 + n2
+    
+    # 2) Criar um método
+    # chamado "subtração()"
+    def subtracao(self, n1, n2):
+        return n1 - n2
+    
+    # 2) Criar um método
+    # chamado "multiplicação()"
+    def multiplicacao(self, n1, n2):
+        return n1 * n2
+    
+    # 2) Criar um método
+    # chamado "diviisao()"
+    def divisao(self, n1, n2):
+        if n2 == 0:
+            return "Divisão inválida"
+        else: 
+            return n1 / n2
+       
+
